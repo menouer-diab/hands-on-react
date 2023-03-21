aa563de7fe8ef83510a425ca002dfbd31a8ea79d
Menouer Diab <Menouer.Diab@hamburgsud.com>
2023-03-21 10:06:44 +0100

    changes made on 21 March 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import ProjectsPage from "./projects/ProjectsPage";
 
 function App() {
   return (
-    <div className="App">
+    /*     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,9 +17,19 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Menouer Please!
         </a>
       </header>
+    </div> */
+/*     <blockquote cite="Seeker After Knowledge">
+      <ul>
+        <li>Tell me and I forget</li>
+        <li>Teach me and I may remember</li>
+        <li>Involve me and I learn</li>
+      </ul>
+    </blockquote> */
+    <div className="container">
+      <ProjectsPage />
     </div>
   );
 }
---
+++ src/projects/ProjectsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProjectsPage() {
+    return (
+        <h1>My New Projects Component</h1>
+    );
+}
+
+export default ProjectsPage;
